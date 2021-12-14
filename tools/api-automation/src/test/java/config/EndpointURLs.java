54a4a67230c30943103640022c44676ba39faec4
naveen zehra <naveen.zehra@simplifipay.com>
2021-12-14 13:21:23 +0500

    restassured and cucumber integration

---
+++ tools/api-automation/src/test/java/config/EndpointURLs.java
@@ -0,0 +1,11 @@
+
+package config;
+
+
+public class EndpointURLs {
+
+
+
+
+
+}
---
+++ tools/api-automation/src/test/java/config/EnvGlobals.java
@@ -0,0 +1,15 @@
+/*
+Define global variables to use re-use them
+ */
+
+package config;
+
+import io.restassured.specification.RequestSpecification;
+
+public class EnvGlobals {
+
+    public static String apiToken;
+    public static String tempToken;
+    public static RequestSpecification requestSpecification;
+
+ }
