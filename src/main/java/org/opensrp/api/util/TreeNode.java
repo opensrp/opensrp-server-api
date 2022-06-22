d26c1de468d07e5a29e2b66f8c59894fb1d82843
sharon2719 <otiennosharon@gmail.com>
2022-06-22 19:22:38 +0300

    Code cleanup and reformat for opensrp-server-api

--- src/main/java/org/opensrp/api/util/TreeNode.java
+++ src/main/java/org/opensrp/api/util/TreeNode.java
@@ -1,103 +1,102 @@
 package org.opensrp.api.util;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class TreeNode<K, T> implements Serializable {
-	
-	private K id;
-	
-	private String label;
-	
-	private T node;
-	
-	private Map<K, TreeNode<K, T>> children;
-	
-	private K parent;
-	
-	public TreeNode(K id, String label, T node, K parent) {
-		this.id = id;
-		this.label = label;
-		this.node = node;
-		this.parent = parent;
-	}
-	
-	public TreeNode(K id, String label, T node, K parent, Map<K, TreeNode<K, T>> children) {
-		this.id = id;
-		this.label = label;
-		this.node = node;
-		this.parent = parent;
-		this.children = children;
-	}
-	
-	public void addChild(TreeNode<K, T> node) {
-		if (children == null) {
-			children = new LinkedHashMap<>();
-		}
-		children.put(node.getId(), node);
-	}
-	
-	public TreeNode<K, T> findChild(K id) {
-		if (children != null) {
-			for (TreeNode<K, T> child : children.values()) {
-				if (child.getId().equals(id)) {
-					return child;
-				} else if (child.getChildren() != null) {
-					TreeNode<K, T> node = child.findChild(id);
-					if (node != null)
-						return node;
-				}
-			}
-		}
-		return null;
-	}
-	
-	public TreeNode<K, T> removeChild(K id) {
-		if (children != null) {
-			for (TreeNode<K, T> child : children.values()) {
-				if (child.getId().equals(id)) {
-					return children.remove(id);
-				} else if (child.getChildren() != null) {
-					TreeNode<K, T> node = child.removeChild(id);
-					if (node != null)
-						return node;
-				}
-			}
-		}
-		return null;
-	}
-	
-	public K getId() {
-		return id;
-	}
-	
-	public String getLabel() {
-		return label;
-	}
-	
-	public T getNode() {
-		return node;
-	}
-	
-	public K getParent() {
-		return parent;
-	}
-	
-	public Map<K, TreeNode<K, T>> getChildren() {
-		return children;
-	}
-	
-	void setLabel(String label) {
-		this.label = label;
-	}
-	
-	void setNode(T node) {
-		this.node = node;
-	}
-	
-	void setParent(K parent) {
-		this.parent = parent;
-	}
+
+    private K id;
+
+    private String label;
+
+    private T node;
+
+    private Map<K, TreeNode<K, T>> children;
+
+    private K parent;
+
+    public TreeNode(K id, String label, T node, K parent) {
+        this.id = id;
+        this.label = label;
+        this.node = node;
+        this.parent = parent;
+    }
+
+    public TreeNode(K id, String label, T node, K parent, Map<K, TreeNode<K, T>> children) {
+        this.id = id;
+        this.label = label;
+        this.node = node;
+        this.parent = parent;
+        this.children = children;
+    }
+
+    public void addChild(TreeNode<K, T> node) {
+        if (children == null) {
+            children = new LinkedHashMap<>();
+        }
+        children.put(node.getId(), node);
+    }
+
+    public TreeNode<K, T> findChild(K id) {
+        if (children != null) {
+            for (TreeNode<K, T> child : children.values()) {
+                if (child.getId().equals(id)) {
+                    return child;
+                } else if (child.getChildren() != null) {
+                    TreeNode<K, T> node = child.findChild(id);
+                    if (node != null)
+                        return node;
+                }
+            }
+        }
+        return null;
+    }
+
+    public TreeNode<K, T> removeChild(K id) {
+        if (children != null) {
+            for (TreeNode<K, T> child : children.values()) {
+                if (child.getId().equals(id)) {
+                    return children.remove(id);
+                } else if (child.getChildren() != null) {
+                    TreeNode<K, T> node = child.removeChild(id);
+                    if (node != null)
+                        return node;
+                }
+            }
+        }
+        return null;
+    }
+
+    public K getId() {
+        return id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    void setLabel(String label) {
+        this.label = label;
+    }
+
+    public T getNode() {
+        return node;
+    }
+
+    void setNode(T node) {
+        this.node = node;
+    }
+
+    public K getParent() {
+        return parent;
+    }
+
+    void setParent(K parent) {
+        this.parent = parent;
+    }
+
+    public Map<K, TreeNode<K, T>> getChildren() {
+        return children;
+    }
 }
--- src/test/java/org/opensrp/api/domain/TimeTest.java
+++ src/test/java/org/opensrp/api/domain/TimeTest.java
@@ -1,11 +1,5 @@
 package org.opensrp.api.domain;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
-import org.junit.Test;
-
 import com.openpojo.reflection.PojoClass;
 import com.openpojo.reflection.construct.InstanceFactory;
 import com.openpojo.reflection.impl.PojoClassFactory;
@@ -13,23 +7,29 @@ import com.openpojo.validation.affirm.Affirm;
 
 import junit.framework.Assert;
 
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class TimeTest {
-	
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
-	@Test
-	@SuppressWarnings("RedundantArrayCreation")
-	public void shouldTestConstructorUsingDefaultConstructor() {
-		final Class<?> clazz = Time.class;
-		final Object obj1 = getInstance(clazz, new Date(), TimeZone.getTimeZone("BDT"));
-		Affirm.affirmNotNull("Should have created an object", obj1);
-		final Time time = (Time) getInstance(clazz, new Date(), TimeZone.getTimeZone("BDT"));
-		Assert.assertEquals(DATE_FORMAT.format(new Date()), time.getTime());
-	}
-	
-	private Object getInstance(final Class<?> clazz, final Object... parameters) {
-		final PojoClass pojoClass = PojoClassFactory.getPojoClass(clazz);
-		return InstanceFactory.getInstance(pojoClass, parameters);
-	}
-	
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Test
+    @SuppressWarnings("RedundantArrayCreation")
+    public void shouldTestConstructorUsingDefaultConstructor() {
+        final Class<?> clazz = Time.class;
+        final Object obj1 = getInstance(clazz, new Date(), TimeZone.getTimeZone("BDT"));
+        Affirm.affirmNotNull("Should have created an object", obj1);
+        final Time time = (Time) getInstance(clazz, new Date(), TimeZone.getTimeZone("BDT"));
+        Assert.assertEquals(DATE_FORMAT.format(new Date()), time.getTime());
+    }
+
+    private Object getInstance(final Class<?> clazz, final Object... parameters) {
+        final PojoClass pojoClass = PojoClassFactory.getPojoClass(clazz);
+        return InstanceFactory.getInstance(pojoClass, parameters);
+    }
+
 }
